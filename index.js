401ef5c25279bbcf26b2c3304b0dbe5b8c204e58
Aayus <godlord@AayushPathak>
2023-11-04 13:31:00 +0530

    signin done

--- index.js
+++ index.js
@@ -14,7 +14,18 @@ app.get("/signup", (req, res) => {
 app.get("/login" , (req,res) => {
     res.render("signin")
 })
-
+app.get("/logine", (req,res) => {
+    res.render("logine")
+})
+app.get("/logins", (req,res) => {
+    res.render("logins")
+})
+app.get("/signupe", (req,res) => {
+    res.render("signupe")
+})  
+app.get("/signups", (req,res) => {
+    res.render("signups")
+})
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
