2be9f4a5d259f88e84cd9fffce3766ba56faa779
Aayus <godlord@AayushPathak>
2023-11-15 13:17:05 +0530

    bug fix

--- migrations/20231108133411-add-user-id-in-chapters.js
+++ migrations/20231108133411-add-user-id-in-chapters.js
@@ -3,8 +3,8 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.addColumn('Chapters',  {
-      type: Sequelize.DataTypes.INTEGER,
+    await queryInterface.addColumn('Chapters', 'courseId', {
+      type: Sequelize.DataTypes.INTEGER,  
     }); 
     await queryInterface.addConstraint("Chapters", {
       fields: ["courseId"],
