4fbe99abb5cb0f05df724d06401ae66c30b2334c
Aayus <godlord@AayushPathak>
2023-11-15 22:13:48 +0530

    fixed corse name  bug

--- app.js
+++ app.js
@@ -366,7 +366,7 @@ app.get(
         return response.status(404).json({ message: "User not found" });
       }
       const userCourses = await Courses.getCourses();
-      // console.log("list course=>",userCourses);
+      // console.log("list course=>",userCourses); 
       response.render("courses", {
         title: "Courses",
         courses: userCourses,
@@ -444,7 +444,7 @@ app.get(
       const coursesWithEnrollmentCount = await Promise.all(
         userCourses.map(async (course) => {
           const enrollmentCount = await course.countEnrollments();
-          return { ...course, enrollmentCount };
+          return { course, enrollmentCount };
         })
       );
 
